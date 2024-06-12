678facc83d12bd985d7e7d9bfdcf48840c82d52d
cheng <chengyihengok@126.com>
2024-06-12 16:18:02 +0800

    add MenuBar

---
+++ react-in-mcintosh/src/App.js
@@ -0,0 +1,12 @@
+import MenuBar from './components/MenuBar';
+import './styles/App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <MenuBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-in-mcintosh/src/components/MenuBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/MenuBar.css'
+
+const MenuBar = () => {
+    return (
+        <div className="menu-bar">
+            <span>File</span>
+            <span>Edit</span>
+            <span>View</span>
+            <span>Label</span>
+            <span>Special</span>
+        </div>
+    );
+};
+export default MenuBar;
