bbf9997c9f5eda00ff27ac57a1b2f16b4184a807
cheng <chengyihengok@126.com>
2024-06-12 16:51:27 +0800

    add logo svg

--- react-in-mcintosh/src/components/MenuBar.js
+++ react-in-mcintosh/src/components/MenuBar.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import '../styles/MenuBar.css'
+import '../styles/MenuBar.css';
+import Logo from '../assets/icons/apple-logo.svg';
 
 const MenuBar = () => {
     return (
         <div className="menu-bar">
+            <span className="menu-img">
+                <img src={Logo} alt="File" className="menu-icon" />
+            </span>
             <span>File</span>
             <span>Edit</span>
             <span>View</span>
